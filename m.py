8ee3f39ac6d32396efecb94b4bec97167024c445
UnityGabbar <mynewlearningid@gmail.com>
2024-06-20 09:45:59 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7397098532:AAGgNh0JpaGNQlGNHSGTNlyuz3kjCx9CbGI')
 
 # Admin user IDs
-admin_id = ["YOUR ID"]
+admin_id = ["1918984956"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
